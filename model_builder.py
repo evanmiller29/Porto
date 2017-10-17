fdcfab2daa1b4f5bb88ba8c0f541ee9bf98861e9
evanmiller29 <evanmiller29@gmail.com>
2017-10-17 21:52:06 +1300

    xgboost switch

--- model_builder.py
+++ model_builder.py
@@ -8,13 +8,36 @@ Created on Tue Oct 17 15:46:05 2017
 import pandas as pd
 import os
 import numpy as np
-from gini import gini_normalized
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import make_scorer
-from catboost import CatBoostClassifier, Pool
+import xgboost as xgb
 from datetime import datetime
 
-gini_sklearn = make_scorer(gini_normalized, True, True)
+#==============================================================================
+# Defining loss function
+#==============================================================================
+
+def gini(actual, pred, cmpcol = 0, sortcol = 1):
+     assert( len(actual) == len(pred) )
+     all = np.asarray(np.c_[ actual, pred, np.arange(len(actual)) ], dtype=np.float)
+     all = all[ np.lexsort((all[:,2], -1*all[:,1])) ]
+     totalLosses = all[:,0].sum()
+     giniSum = all[:,0].cumsum().sum() / totalLosses
+ 
+     giniSum -= (len(actual) + 1) / 2.
+     return giniSum / len(actual)
+ 
+def gini_normalized(a, p):
+     return gini(a, p) / gini(a, a)
+ 
+def gini_xgb(preds, dtrain):
+    labels = dtrain.get_label()
+    gini_score = gini_normalized(labels, preds)
+    return [('gini', gini_score)]
+
+#==============================================================================
+# Reading in data
+#==============================================================================
 
 base_dir = 'C:/Users/Evan/Documents/GitHub/Data/Porto'
 sub_dir = 'F:/Nerdy Stuff/Kaggle/Porto/Submissions'
@@ -57,23 +80,44 @@ print('Train samples: {} Validation samples: {}'.format(len(x_train), len(x_vali
 x_train = x_train.fillna(999)
 x_valid = x_valid.fillna(999)
 
-train_pool = Pool(data = x_train, label = y_train, cat_features = cat_cols_idx)
-valid_pool = Pool(data = x_valid, label = y_valid, cat_features = cat_cols_idx)
+d_train = xgb.DMatrix(x_train, y_train)
+d_valid = xgb.DMatrix(x_valid, y_valid)
+d_test = xgb.DMatrix(x_test)
 
-model = CatBoostClassifier(iterations=10, learning_rate=0.01, verbose=True, custom_loss = gini_sklearn)
-model.fit(train_pool)
+# Set xgboost parameters
+params = {}
+params['objective'] = 'binary:logistic'
+params['booster'] = 'dart'
+params['eta'] = 0.02
+params['silent'] = True
+params['max_depth'] = 6
+params['subsample'] = 0.9
+params['colsample_bytree'] = 0.9
 
-pred_valid = model.predict_proba(x_valid)[:, 1]
+cv_params = {'max_depth': [3,5,7], 'min_child_weight': [1,3,5]}
+
+watchlist = [(d_train, 'train'), (d_valid, 'valid')]
+
+# Train the model! We pass in a max of 10,000 rounds (with early stopping after 100)
+# and the custom metric (maximize=True tells xgb that higher metric is better)
+
+mdl = xgb.train(params, d_train, 1000, watchlist, early_stopping_rounds=100, 
+                feval=gini_xgb, maximize=True, verbose_eval=10)
+
+pred_valid = mdl.predict(d_valid)
 gini_norm_valid = gini_normalized(y_valid, pred_valid)
 
 print('gini (normalised) for the validation set %s' % (round(gini_norm_valid , 4)))
-preds = model.predict_proba(x_test)[:, 1]
+
+p_test = mdl.predict(d_test)
+
+print('Creating a submission file..')
 
 os.chdir(sub_dir)
 
 sub = pd.DataFrame()
 sub['id'] = test_id
-sub['target'] = preds
+sub['target'] = p_test
 
 sub.to_csv(sample.to_csv('sub{}_{}.csv'.format(datetime.now().strftime('%Y%m%d_%H%M%S'), gini_norm_valid),
                          index=False, float_format='%.4f'), index=False)
