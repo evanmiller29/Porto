9ff0b03c4bcb9055f082a99392f71da42ae23a3a
evanmiller29 <evanmiller29@gmail.com>
2017-10-17 16:06:41 +1300

    adding gini coeff

--- model_builder.py
+++ model_builder.py
@@ -13,3 +13,18 @@ os.chdir(base_dir)
 
 train = pd.read_csv('train.csv', low_memory = True)
 test = pd.read_csv('test.csv', low_memory = True)
+
+train_columns = list(train.columns.values)
+test_columns = list(test.columns.values)
+
+print(list(set(train_columns) - set(test_columns)))
+
+y_train = train['target'].values
+x_train = train.drop('target', axis = 1)
+
+y_test= test['target'].values
+x_test = test.drop('target', axis = 1)
+
+binary_cols = [i for e in ['bin'] for i in list(train_columns) if e in i]
+cat_cols = [i for e in ['cat'] for i in list(train_columns) if e in i]
+
