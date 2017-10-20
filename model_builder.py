054d57f5381b70f3c7afef6ef91ff46957168345
evanmiller29 <evanmiller29@gmail.com>
2017-10-21 10:42:32 +1300

    Getting catboost working

--- model_builder.py
+++ model_builder.py
@@ -9,13 +9,23 @@ import pandas as pd
 import os
 import numpy as np
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import make_scorer
-import xgboost as xgb
+from catboost import CatBoostClassifier, Pool
 from datetime import datetime
 
 #==============================================================================
 # Defining loss function
 #==============================================================================
+
+
+def gini(y, pred):
+    g = np.asarray(np.c_[y, pred, np.arange(len(y)) ], dtype=np.float)
+    g = g[np.lexsort((g[:,2], -1*g[:,1]))]
+    gs = g[:,0].cumsum().sum() / g[:,0].sum()
+    gs -= (len(y) + 1) / 2.
+    return gs / len(y)
+
+def gini_catboost(pred, y):
+    return gini(y, pred) / gini(y, y)
 
 def gini(actual, pred, cmpcol = 0, sortcol = 1):
      assert( len(actual) == len(pred) )
@@ -28,12 +38,7 @@ def gini(actual, pred, cmpcol = 0, sortcol = 1):
      return giniSum / len(actual)
  
 def gini_normalized(a, p):
-     return gini(a, p) / gini(a, a)
- 
-def gini_xgb(preds, dtrain):
-    labels = dtrain.get_label()
-    gini_score = gini_normalized(labels, preds)
-    return [('gini', gini_score)]
+    return gini(a, p) / gini(a, a)
 
 #==============================================================================
 # Reading in data
@@ -65,14 +70,8 @@ train_cols_up = x_train.columns.values
 
 binary_cols = [i for e in ['bin'] for i in train_cols_up  if e in i]
 cat_cols = [i for e in ['cat'] for i in train_cols_up  if e in i]
-ind_cols = [i for e in ['ind'] for i in train_cols_up  if e in i]
-reg_cols = [i for e in ['reg'] for i in train_cols_up  if e in i]
-car_cols = [i for e in ['car'] for i in train_cols_up  if e in i]
 
 cat_cols_idx = [i for i, c in enumerate(train_cols_up) if 'cat' in c]
-
-ttl_used = binary_cols + cat_cols + ind_cols + reg_cols + car_cols
-calc_cols = [col for col in train_cols_up if col not in ttl_used]
 
 x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.2, random_state=4242)
 print('Train samples: {} Validation samples: {}'.format(len(x_train), len(x_valid)))
@@ -80,43 +79,28 @@ print('Train samples: {} Validation samples: {}'.format(len(x_train), len(x_vali
 x_train = x_train.fillna(999)
 x_valid = x_valid.fillna(999)
 
-d_train = xgb.DMatrix(x_train, y_train)
-d_valid = xgb.DMatrix(x_valid, y_valid)
-d_test = xgb.DMatrix(x_test)
+print('training catboost..')
 
-# Set xgboost parameters
-params = {}
-params['objective'] = 'binary:logistic'
-params['booster'] = 'dart'
-params['eta'] = 0.02
-params['silent'] = True
-params['max_depth'] = 6
-params['subsample'] = 0.9
-params['colsample_bytree'] = 0.9
+train_pool = Pool(x_train, y_train, cat_features=cat_cols_idx)
+valid_pool = Pool(x_valid, y_valid, cat_features=cat_cols_idx)
 
-cv_params = {'max_depth': [3,5,7], 'min_child_weight': [1,3,5]}
+model = CatBoostClassifier(iterations=2, learning_rate=1, depth=2, custom_loss='AUC')
+fit_model = model.fit(train_pool)
 
-watchlist = [(d_train, 'train'), (d_valid, 'valid')]
+print('predicting model outputs..')
+y_pred = fit_model.predict_proba(x_test)[:,1]
 
-# Train the model! We pass in a max of 10,000 rounds (with early stopping after 100)
-# and the custom metric (maximize=True tells xgb that higher metric is better)
-
-mdl = xgb.train(params, d_train, 500, watchlist, early_stopping_rounds=100, 
-                feval=gini_xgb, maximize=True, verbose_eval=100)
-
-pred_valid = mdl.predict(d_valid)
+pred_valid = model.predict(valid_pool)
 gini_norm_valid = round(gini_normalized(y_valid, pred_valid), 4)
 
 print('gini (normalised) for the validation set %s' % (gini_norm_valid))
 
-p_test = mdl.predict(d_test)
-
-print('Creating a submission file..')
 os.chdir(sub_dir)
 
+# Create a submission file
 sub = pd.DataFrame()
 sub['id'] = test_id
-sub['target'] = p_test
+sub['target'] = y_pred
+sub.to_csv('sub{}_{}.csv'.format(datetime.now().strftime('%Y%m%d_%H%M%S'), gini_norm_valid),
+                         index=False, float_format='%.4f')
 
-sub.to_csv(sample.to_csv('sub{}_{}.csv'.format(datetime.now().strftime('%Y%m%d_%H%M%S'), gini_norm_valid),
-                         index=False, float_format='%.4f'), index=False)
