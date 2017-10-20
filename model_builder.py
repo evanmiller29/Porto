00f3a57fcb373506addced13449dc20e24518677
evanmiller29 <evanmiller29@gmail.com>
2017-10-21 11:30:08 +1300

    Implementing k-fold GridSearch

--- model_builder.py
+++ model_builder.py
@@ -8,24 +8,14 @@ Created on Tue Oct 17 15:46:05 2017
 import pandas as pd
 import os
 import numpy as np
-from sklearn.cross_validation import train_test_split
-from catboost import CatBoostClassifier, Pool
+import catboost as cb
+from sklearn.model_selection import KFold
 from datetime import datetime
+from itertools import product,chain
 
 #==============================================================================
-# Defining loss function
+# Defining custom functions
 #==============================================================================
-
-
-def gini(y, pred):
-    g = np.asarray(np.c_[y, pred, np.arange(len(y)) ], dtype=np.float)
-    g = g[np.lexsort((g[:,2], -1*g[:,1]))]
-    gs = g[:,0].cumsum().sum() / g[:,0].sum()
-    gs -= (len(y) + 1) / 2.
-    return gs / len(y)
-
-def gini_catboost(pred, y):
-    return gini(y, pred) / gini(y, y)
 
 def gini(actual, pred, cmpcol = 0, sortcol = 1):
      assert( len(actual) == len(pred) )
@@ -40,12 +30,52 @@ def gini(actual, pred, cmpcol = 0, sortcol = 1):
 def gini_normalized(a, p):
     return gini(a, p) / gini(a, a)
 
+# this function does 3-fold crossvalidation with catboostclassifier          
+def crossvaltest(params,train_set,train_label,cat_dims,n_splits=3):
+    kf = KFold(n_splits=n_splits,shuffle=True) 
+    res = []
+    for train_index, test_index in kf.split(train_set):
+        train = train_set.iloc[train_index,:]
+        test = train_set.iloc[test_index,:]
+
+        labels = train_label.ix[train_index]
+        test_labels = train_label.ix[test_index]
+
+        clf = cb.CatBoostClassifier(**params)
+        clf.fit(train, np.ravel(labels), cat_features=cat_dims)
+
+        res.append(np.mean(clf.predict(test)==np.ravel(test_labels)))
+    return np.mean(res)
+
+# this function runs grid search on several parameters
+def catboost_param_tune(params,train_set,train_label,cat_dims=None,n_splits=3):
+    ps = paramsearch(params)
+    # search 'border_count', 'l2_leaf_reg' etc. individually 
+    #   but 'iterations','learning_rate' together
+    for prms in chain(ps.grid_search(['border_count']),
+                      ps.grid_search(['ctr_border_count']),
+                      ps.grid_search(['l2_leaf_reg']),
+                      ps.grid_search(['iterations','learning_rate']),
+                      ps.grid_search(['depth'])):
+        res = crossvaltest(prms,train_set,train_label,cat_dims,n_splits)
+        # save the crossvalidation result so that future iterations can reuse the best parameters
+        ps.register_result(res,prms)
+        print(res,prms,'best:',ps.bestscore(),ps.bestparam())
+        
+    return ps.bestparam()
+
+
 #==============================================================================
 # Reading in data
 #==============================================================================
 
 base_dir = 'C:/Users/Evan/Documents/GitHub/Data/Porto'
 sub_dir = 'F:/Nerdy Stuff/Kaggle submissions/Porto'
+script_dir = 'C:/Users/Evan/Documents/GitHub/Porto'
+
+os.chdir(script_dir)
+
+from paramsearch import paramsearch
 
 os.chdir(base_dir)
 
@@ -58,12 +88,12 @@ test_columns = list(test.columns.values)
 
 print('Base cols not in either set: %s' % (list(set(train_columns) - set(test_columns))))
 
-y_train = train['target'].values
-train_id = train['id'].values
+y_train = train['target']
+train_id = train['id']
 x_train = train.drop(['target', 'id'], axis = 1)
 
 y_test = []
-test_id = test['id'].values
+test_id = test['id']
 x_test = test.drop('id', axis = 1)
 
 train_cols_up = x_train.columns.values
@@ -73,16 +103,10 @@ cat_cols = [i for e in ['cat'] for i in train_cols_up  if e in i]
 
 cat_cols_idx = [i for i, c in enumerate(train_cols_up) if 'cat' in c]
 
-x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.2, random_state=4242)
-print('Train samples: {} Validation samples: {}'.format(len(x_train), len(x_valid)))
-
+train = train.fillna(999)
 x_train = x_train.fillna(999)
-x_valid = x_valid.fillna(999)
 
 print('training catboost..')
-
-train_pool = Pool(x_train, y_train, cat_features=cat_cols_idx)
-valid_pool = Pool(x_valid, y_valid, cat_features=cat_cols_idx)
 
 params_old = {'iterations': 2,
           'learning_rate': 1,
@@ -98,15 +122,24 @@ params_new = {'depth':[3,1,2,6,4,5,7,8,9,10],
           'thread_count':4,
           'custom_loss': 'AUC'}
 
-params_set = [params_old, params_new]
 
-model = CatBoostClassifier(**params_old)
-fit_model = model.fit(train_pool)
+bestparams = catboost_param_tune(params_new, x_train,
+                                 y_train, cat_cols_idx)
+
+
+# train classifier with tuned parameters    
+clf = cb.CatBoostClassifier(**bestparams)
+clf.fit(x_train, np.ravel(y_train), cat_features=cat_cols_idx)
+
+# How do I want to do this last section? Just a random cut of data?
+# print('error:',1-np.mean(res==np.ravel(test_label)))
 
 print('predicting model outputs..')
 y_pred = fit_model.predict_proba(x_test)[:,1]
 
 pred_valid = model.predict(valid_pool)
+
+#I'd prefer to do K-fold here with n = 5 to get the better understanding of the error
 gini_norm_valid = round(gini_normalized(y_valid, pred_valid), 4)
 
 print('gini (normalised) for the validation set %s' % (gini_norm_valid))
