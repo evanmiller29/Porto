486206485e4aec4aeb7c1295fa8482d0470a0a27
evanmiller29 <evanmiller29@gmail.com>
2017-10-23 09:23:28 +1300

    Adding in better gini calcs

--- model_builder.py
+++ model_builder.py
@@ -9,43 +9,29 @@ import pandas as pd
 import os
 import numpy as np
 import catboost as cb
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from datetime import datetime
-from itertools import product,chain
+from numba import jit
 
 #==============================================================================
 # Defining custom functions
 #==============================================================================
 
-def gini(actual, pred, cmpcol = 0, sortcol = 1):
-     assert( len(actual) == len(pred) )
-     all = np.asarray(np.c_[ actual, pred, np.arange(len(actual)) ], dtype=np.float)
-     all = all[ np.lexsort((all[:,2], -1*all[:,1])) ]
-     totalLosses = all[:,0].sum()
-     giniSum = all[:,0].cumsum().sum() / totalLosses
- 
-     giniSum -= (len(actual) + 1) / 2.
-     return giniSum / len(actual)
- 
-def gini_normalized(a, p):
-    return gini(a, p) / gini(a, a)
-
-# this function does 3-fold crossvalidation with catboostclassifier          
-def crossvaltest(params,train_set,train_label,cat_dims,n_splits=3):
-    kf = KFold(n_splits=n_splits,shuffle=True) 
-    res = []
-    for train_index, test_index in kf.split(train_set):
-        train = train_set.iloc[train_index,:]
-        test = train_set.iloc[test_index,:]
-
-        labels = train_label.ix[train_index]
-        test_labels = train_label.ix[test_index]
-
-        clf = cb.CatBoostClassifier(**params)
-        clf.fit(train, np.ravel(labels), cat_features=cat_dims)
-
-        res.append(np.mean(clf.predict(test)==np.ravel(test_labels)))
-    return np.mean(res)
+@jit
+def eval_gini(y_true, y_prob):
+    y_true = np.asarray(y_true)
+    y_true = y_true[np.argsort(y_prob)]
+    ntrue = 0
+    gini = 0
+    delta = 0
+    n = len(y_true)
+    for i in range(n-1, -1, -1):
+        y_i = y_true[i]
+        ntrue += y_i
+        gini += y_i * delta
+        delta += 1 - y_i
+    gini = 1 - 2 * gini / (ntrue * (n - ntrue))
+    return gini
 
 #==============================================================================
 # Reading in data
@@ -55,15 +41,15 @@ base_dir = 'C:/Users/Evan/Documents/GitHub/Data/Porto'
 sub_dir = 'F:/Nerdy Stuff/Kaggle submissions/Porto'
 script_dir = 'C:/Users/Evan/Documents/GitHub/Porto'
 
-os.chdir(script_dir)
-
-from paramsearch import paramsearch
-
 os.chdir(base_dir)
 
 train = pd.read_csv('train.csv', low_memory = True)
 test = pd.read_csv('test.csv', low_memory = True)
 sample = pd.read_csv('sample_submission.csv', low_memory = True)
+
+# =============================================================================
+# Splitting dataframes
+# =============================================================================
 
 train_columns = list(train.columns.values)
 test_columns = list(test.columns.values)
@@ -85,26 +71,33 @@ cat_cols = [i for e in ['cat'] for i in train_cols_up  if e in i]
 
 cat_cols_idx = [i for i, c in enumerate(train_cols_up) if 'cat' in c]
 
+# =============================================================================
+# Formatting data for catboost
+# =============================================================================
+
 x_train = x_train.fillna(999)
+x_test = x_test.fillna(999)
 
 X = x_train.values
 y = y_train.values
 
 print('training catboost..')
 
-params = {'depth':2,
-          'iterations':100,
-          'learning_rate':0.001, 
-          'l2_leaf_reg':5,
-          'border_count':10,
-          'ctr_border_count':50,
-          'thread_count':5,
-          'custom_loss': 'Logloss'}
+modelMeta = dict()
+modelMeta['MAX_ROUNDS'] = 650
+modelMeta['OPTIMIZE_ROUNDS'] = False
+modelMeta['LEARNING_RATE'] = 0.05
+
+params = {'learning_rate': modelMeta['LEARNING_RATE'], 
+            'depth': 6, 
+            'l2_leaf_reg' : 8, 
+            'iterations' : modelMeta['MAX_ROUNDS'],
+            'border_count':10,
+            'ctr_border_count':50,
+            'loss_function' : 'Logloss'}
 
 kfold = 3
 skf = StratifiedKFold(n_splits=kfold, random_state=42)
-
-os.chdir(sub_dir)
 
 sub = pd.DataFrame()
 sub['id'] = test_id
@@ -123,24 +116,41 @@ for i, (train_index, test_index) in enumerate(skf.split(X, y)):
     valid_pool = cb.Pool(X_valid, y_valid, cat_cols_idx)
     
     clf = cb.CatBoostClassifier(**params)
-    clf.fit(train_pool)
-    valid_preds = clf.predict(valid_pool)
     
-    gini_norm_valid = round(gini_normalized(y_valid, valid_preds), 4)
+    if modelMeta['OPTIMIZE_ROUNDS']:
+        
+        fit_model = clf.fit(train_pool,
+                            eval_set = valid_pool,
+                            use_best_model = True
+                            )
+        
+        print( "  N trees = ", clf.tree_count_ )
+        
+    else:
+        
+        fit_model = clf.fit(train_pool)
+    
+    valid_preds = fit_model.predict(valid_pool)
+    
+    gini_norm_valid = round(eval_gini(y_valid, valid_preds), 4)
 
     print('[Fold %d/%d Prediciton:]' % (i + 1, kfold))
     print('Accuracy on validation set: %s' % (gini_norm_valid))
     
     print('predicting model outputs..')
     
-    p_test = clf.predict_proba(x_test)[:, 1]
+    p_test = fit_model.predict_proba(x_test)[:, 1]
     sub['target'] += p_test/kfold
 
     gini_res.append(gini_norm_valid)
 
-# Create a submission file
+# =============================================================================
+# Outputting results to a submission file
+# =============================================================================
 
 avg_gini = str(round(np.mean(gini_res), 4))[0:10]
+
+os.chdir(sub_dir)
 
 sub.to_csv('sub{}_{}.csv'.format(datetime.now().strftime('%Y%m%d_%H%M%S'), avg_gini),
                          index=False, float_format='%.4f')
