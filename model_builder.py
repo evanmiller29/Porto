b76969d70e654dd18d2b8bc922640868496c01a2
evanmiller29 <evanmiller29@gmail.com>
2017-10-23 21:06:19 +1300

    Re-doing catboost

--- model_builder.py
+++ model_builder.py
@@ -1,22 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 17 15:46:05 2017
-
-@author: Evan
-"""
-
-import pandas as pd
-import os
 import numpy as np
-import catboost as cb
-from sklearn.model_selection import StratifiedKFold
+import pandas as pd
+from catboost import CatBoostClassifier
+from sklearn.model_selection import KFold
 from datetime import datetime
 from numba import jit
+import os
 
-#==============================================================================
-# Defining custom functions
-#==============================================================================
+# Compute gini
 
+# from CPMP's kernel https://www.kaggle.com/cpmpml/extremely-fast-gini-computation
 @jit
 def eval_gini(y_true, y_prob):
     y_true = np.asarray(y_true)
@@ -33,125 +25,107 @@ def eval_gini(y_true, y_prob):
     gini = 1 - 2 * gini / (ntrue * (n - ntrue))
     return gini
 
-#==============================================================================
-# Reading in data
-#==============================================================================
-
 base_dir = 'C:/Users/Evan/Documents/GitHub/Data/Porto'
 sub_dir = 'F:/Nerdy Stuff/Kaggle submissions/Porto'
 script_dir = 'C:/Users/Evan/Documents/GitHub/Porto'
 
 os.chdir(base_dir)
 
-train = pd.read_csv('train.csv', low_memory = True)
-test = pd.read_csv('test.csv', low_memory = True)
+runDesc = dict()
+
+runDesc['MAX_ROUNDS'] = 650
+runDesc['OPTIMIZE_ROUNDS'] = False
+runDesc['LEARNING_RATE'] = 0.05
+
+train_df = pd.read_csv('train.csv', low_memory = True)
+test_df = pd.read_csv('test.csv', low_memory = True)
 sample = pd.read_csv('sample_submission.csv', low_memory = True)
 
-# =============================================================================
-# Splitting dataframes
-# =============================================================================
+# Process data
+id_test = test_df['id'].values
+id_train = train_df['id'].values
 
-train_columns = list(train.columns.values)
-test_columns = list(test.columns.values)
+train_df = train_df.fillna(999)
+test_df = test_df.fillna(999)
 
-print('Base cols not in either set: %s' % (list(set(train_columns) - set(test_columns))))
+col_to_drop = train_df.columns[train_df.columns.str.startswith('ps_calc_')]
+cat_cols = [i for e in ['cat'] for i in train_df.columns  if e in i]
+cat_cols_idx = [i for i, c in enumerate(train_df.columns) if 'cat' in c]
 
-y_train = train['target']
-train_id = train['id']
-x_train = train.drop(['target', 'id'], axis = 1)
+train_df = train_df.drop(col_to_drop, axis=1)  
+test_df = test_df.drop(col_to_drop, axis=1)  
 
-y_test = []
-test_id = test['id']
-x_test = test.drop('id', axis = 1)
+for c in train_df.select_dtypes(include=['float64']).columns:
+    train_df[c]=train_df[c].astype(np.float32)
+    test_df[c]=test_df[c].astype(np.float32)
 
-train_cols_up = x_train.columns.values
+for c in train_df.select_dtypes(include=['int64']).columns[2:]:
+    train_df[c]=train_df[c].astype(np.int8)
+    test_df[c]=test_df[c].astype(np.int8)
+    
+y = train_df['target']
+X = train_df.drop(['target', 'id'], axis=1)
+y_valid_pred = 0*y
+X_test = test_df.drop(['id'], axis=1)
+y_test_pred = 0
 
-binary_cols = [i for e in ['bin'] for i in train_cols_up  if e in i]
-cat_cols = [i for e in ['cat'] for i in train_cols_up  if e in i]
-
-cat_cols_idx = [i for i, c in enumerate(train_cols_up) if 'cat' in c]
-
-# =============================================================================
-# Formatting data for catboost
-# =============================================================================
-
-x_train = x_train.fillna(999)
-x_test = x_test.fillna(999)
-
-X = x_train.values
-y = y_train.values
-
-print('training catboost..')
-
-modelMeta = dict()
-modelMeta['MAX_ROUNDS'] = 650
-modelMeta['OPTIMIZE_ROUNDS'] = False
-modelMeta['LEARNING_RATE'] = 0.05
-
-params = {'learning_rate': modelMeta['LEARNING_RATE'], 
-            'depth': 6, 
-            'l2_leaf_reg' : 8, 
-            'iterations' : modelMeta['MAX_ROUNDS'],
-            'border_count':10,
-            'ctr_border_count':50,
-            'loss_function' : 'Logloss'}
-
-kfold = 3
-skf = StratifiedKFold(n_splits=kfold, random_state=42)
-
-sub = pd.DataFrame()
-sub['id'] = test_id
-sub['target'] = np.zeros_like(test_id)
-
+# Set up folds
+K = 5
+kf = KFold(n_splits = K, random_state = 1, shuffle = True)
 gini_res = []
 
-for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+# Set up classifier
+model = CatBoostClassifier(
+    learning_rate = runDesc['LEARNING_RATE'], 
+    depth=6, 
+    l2_leaf_reg = 8, 
+    border_count = 10,
+    ctr_border_count = 50,
+    iterations = runDesc['MAX_ROUNDS'],
+    thread_count = 6,
+    loss_function='Logloss'
+)
+
+# Run CV
+
+for i, (train_index, test_index) in enumerate(kf.split(train_df)):
     
-    print('[Fold %d/%d]' % (i + 1, kfold))
-    X_train, X_valid = X[train_index], X[test_index]
-    y_train, y_valid = y[train_index], y[test_index]
-    # Convert our data into Catboost format
+    # Create data for this fold
+    y_train, y_valid = y.iloc[train_index], y.iloc[test_index]
+    X_train, X_valid = X.iloc[train_index,:], X.iloc[test_index,:]
+    print( "\nFold ", i)
     
-    train_pool = cb.Pool(X_train, y_train, cat_cols_idx)
-    valid_pool = cb.Pool(X_valid, y_valid, cat_cols_idx)
-    
-    clf = cb.CatBoostClassifier(**params)
-    
-    if modelMeta['OPTIMIZE_ROUNDS']:
-        
-        fit_model = clf.fit(train_pool,
-                            eval_set = valid_pool,
-                            use_best_model = True
-                            )
-        
-        print( "  N trees = ", clf.tree_count_ )
-        
+    # Run model for this fold
+    if runDesc['OPTIMIZE_ROUNDS']:
+        fit_model = model.fit( X_train, y_train, cat_features=cat_cols_idx,
+                               eval_set=[X_valid, y_valid],
+                               use_best_model=True
+                             )
+        print( "  N trees = ", model.tree_count_ )
     else:
+        fit_model = model.fit( X_train, y_train )
         
-        fit_model = clf.fit(train_pool)
+    # Generate validation predictions for this fold
+    pred = fit_model.predict_proba(X_valid)[:,1]
+    print( "  Gini = ", eval_gini(y_valid, pred) )
+    y_valid_pred.iloc[test_index] = pred
     
-    valid_preds = fit_model.predict(valid_pool)
+    # Accumulate test set predictions
+    y_test_pred += fit_model.predict_proba(X_test)[:,1]
+    gini_res.append(eval_gini(y_valid, pred))
     
-    gini_norm_valid = round(eval_gini(y_valid, valid_preds), 4)
+y_test_pred /= K  # Average test set predictions
 
-    print('[Fold %d/%d Prediciton:]' % (i + 1, kfold))
-    print('Accuracy on validation set: %s' % (gini_norm_valid))
-    
-    print('predicting model outputs..')
-    
-    p_test = fit_model.predict_proba(x_test)[:, 1]
-    sub['target'] += p_test/kfold
-
-    gini_res.append(gini_norm_valid)
-
-# =============================================================================
-# Outputting results to a submission file
-# =============================================================================
+print( "\nGini for full training set:" )
+print(eval_gini(y, y_valid_pred))
 
 avg_gini = str(round(np.mean(gini_res), 4))[0:10]
 
 os.chdir(sub_dir)
+# Create submission file
+sub = pd.DataFrame()
+sub['id'] = id_test
+sub['target'] = y_test_pred
 
 sub.to_csv('sub{}_{}.csv'.format(datetime.now().strftime('%Y%m%d_%H%M%S'), avg_gini),
-                         index=False, float_format='%.4f')
-
+                         index=False, float_format='%.6f')
