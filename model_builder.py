df5bf7e9514686b29e016c1878b69281b4c00733
evanmiller29 <evanmiller29@gmail.com>
2017-10-24 22:10:02 +1300

    Adding in SMOTE

--- model_builder.py
+++ model_builder.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
 from catboost import CatBoostClassifier
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime
 from numba import jit
 import os
+from imblearn.over_sampling import SMOTE
+from imblearn import pipeline as pl
 
 # Compute gini
 
@@ -36,7 +38,7 @@ runDesc = {'MAX_ROUNDS': 650,
            'OPTIMIZE_ROUNDS': False,
            'LEARNING_RATE': 0.05,
            'K': 5,
-           'CROSS_VAL': False}
+           'CROSS_VAL': True}
 
 train_df = pd.read_csv('train.csv', low_memory = True)
 test_df = pd.read_csv('test.csv', low_memory = True)
@@ -72,9 +74,6 @@ y_test_pred = 0
 
 # Set up folds
 
-if runDesc['CROSS_VAL']:
-
-kf = KFold(n_splits = runDesc['K'], random_state = 1, shuffle = True)
 gini_res = []
 
 params = {'learning_rate' : runDesc['LEARNING_RATE'], 
@@ -88,52 +87,71 @@ params = {'learning_rate' : runDesc['LEARNING_RATE'],
         
         }
 
-
-# Set up classifier
 model = CatBoostClassifier(**params)
-scaler = MinMaxScaler()
 
-# Run CV
+if runDesc['CROSS_VAL']:
 
-for i, (train_index, test_index) in enumerate(kf.split(train_df)):
-    
-    # Create data for this fold
-    y_train, y_valid = y.iloc[train_index], y.iloc[test_index]
-    X_train, X_valid = X.iloc[train_index,:], X.iloc[test_index,:]
-    
-    scaler.fit(X_train)
-    
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-    
-    print( "\nFold ", i)
-    
-    # Run model for this fold
-    if runDesc['OPTIMIZE_ROUNDS']:
-        fit_model = model.fit( X_train, y_train, cat_features=cat_cols_idx,
-                               eval_set=[X_valid, y_valid],
-                               use_best_model=True
-                             )
-        print( "  N trees = ", model.tree_count_ )
-    else:
-        fit_model = model.fit( X_train, y_train )
+    kf = KFold(n_splits = runDesc['K'], random_state = 1, shuffle = True)
+
+    for i, (train_index, test_index) in enumerate(kf.split(train_df)):
         
-    # Generate validation predictions for this fold
+        # Create data for this fold
+        y_train, y_valid = y.iloc[train_index], y.iloc[test_index]
+        X_train, X_valid = X.iloc[train_index,:], X.iloc[test_index,:]
+        
+        X_train, y_train = SMOTE(random_state=42).fit_sample(X_train, y_train)
+        
+        print( "\nFold ", i)
+        
+        # Run model for this fold
+        if runDesc['OPTIMIZE_ROUNDS']:
+            fit_model = model.fit( X_train, y_train, cat_features=cat_cols_idx,
+                                   eval_set=[X_valid, y_valid],
+                                   use_best_model=True
+                                 )
+            print( "  N trees = ", model.tree_count_ )
+        else:
+            fit_model = model.fit( X_train, y_train )
+            
+        # Generate validation predictions for this fold
+        pred = fit_model.predict_proba(X_valid)[:,1]
+        print( "  Gini = ", eval_gini(y_valid, pred) )
+        y_valid_pred.iloc[test_index] = pred
+        
+        # Accumulate test set predictions
+        y_test_pred += fit_model.predict_proba(X_test)[:,1]
+        gini_res.append(eval_gini(y_valid, pred))
+        
+    y_test_pred /= runDesc['K']  # Average test set predictions
+    
+    print( "\nGini for full training set:" )
+    print(eval_gini(y, y_valid_pred))
+    
+    avg_gini = str(round(np.mean(gini_res), 4))[0:10]
+
+if not runDesc['CROSS_VAL']:
+    
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.33, 
+                                                        random_state=42)
+    
+    if runDesc['OPTIMIZE_ROUNDS']:
+            fit_model = model.fit( X_train, y_train, cat_features=cat_cols_idx,
+                                   eval_set=[X_valid, y_valid],
+                                   use_best_model=True
+                                 )
+            print( "  N trees = ", model.tree_count_ )
+    else:
+        
+        X_train, y_train = SMOTE(random_state=42).fit_sample(X_train, y_train)
+        fit_model = model.fit( X_train, y_train )
+            
+        # Generate validation predictions for this fold
     pred = fit_model.predict_proba(X_valid)[:,1]
     print( "  Gini = ", eval_gini(y_valid, pred) )
-    y_valid_pred.iloc[test_index] = pred
+            
+    print( "\nGini for full training set:" )
+    print(eval_gini(y, pred))
     
-    # Accumulate test set predictions
-    y_test_pred += fit_model.predict_proba(X_test)[:,1]
-    gini_res.append(eval_gini(y_valid, pred))
-    
-y_test_pred /= runDesc['K']  # Average test set predictions
-
-print( "\nGini for full training set:" )
-print(eval_gini(y, y_valid_pred))
-
-avg_gini = str(round(np.mean(gini_res), 4))[0:10]
-
 os.chdir(sub_dir)
 # Create submission file
 sub = pd.DataFrame()
